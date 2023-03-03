77357d7579fa265cd5dd8528b22a1620c9a47050
Dimitry995 <98602983+Dimitry995@users.noreply.github.com>
2023-03-03 16:27:18 +0200

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,1 +1,8 @@
 export { NavBar as default } from './NavBar';
+chrome.runtime.onMessage.addListener(function(rq, sender, sendResponse) 
+{
+     setTimeout(function() {
+        sendResponse({status: true});
+    }, 1);
+     return true;  // Add return true to fix the error.
+});
